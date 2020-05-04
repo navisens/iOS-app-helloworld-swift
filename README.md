7eb5f0ca06b3cf89d05191f7da16aaf1d44440bc
James Grantham <james@navisens.com>
2020-05-04 09:26:29 -0700

    updates for 2.0.3

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# iOS-app-helloworld-swift
+# iOS App Hello World (swift)
 An example Swift iOS project using the Navisens MotionDNA SDK
 
-___Note: This app is designed to run on iOS 9.1 or higher___
+___Note: This app is designed to run on iOS 12.0 or higher___
 
 run ```pod install``` from the project folder to install necessary dependencies
 
 ## What it does
-This project builds and runs a bare bones implementation of our SDK core. 
+This project builds and runs a bare bones implementation of our SDK core.
 
 The core is initialized and activated on startup in the view controller triggering a call to the ```startMotionDna``` method in the view controller. After this occurs the controller checks for necessary location permission and if requirements are satisfied, begins to receive Navisens MotionDNA location estimates through the ```receiveMotionDna:``` callback method. The data received is used to update the appropriate label element with a user's relative x,y and z coordinated along with GPS data and motion categorizations.
 
@@ -14,61 +14,103 @@ Before attempting to run this project please be sure you have obtained a develep
 
 For more complete documentation on our SDK please visit our [NaviDocs](https://github.com/navisens/NaviDocs)
 
+### Setup
 
-### Implementation
+Add the subclassed MotionDnaSDK as a property of your View Controller
 
-The Navisens MotionDna SDK is implemented in your project by first subclassing the MotionDnaSDK class and then implementing select callbacks for receiving estimation data and other various pieces of information
+``` Swift
+manager.runMotionDna("<developer-key>")
+```
 
-The structure of this class should resemble the following code and implement a subset of the methods listed.
+Walk around to see your position update.
 
-###### MotionDnaManager.swift
+
+## How the SDK works
+
+Please refer to our [NaviDoc](https://github.com/navisens/NaviDocs/blob/master/API.iOS.md#api) for full documentation.
+
+### How you include (and update) the SDK
+
+Add `pod 'MotionDnaSDK` to your podfile and run `pod install` to load the SDK. If there has been a version update to the SDK recently then run `pod update` from the project folder to update the repo.
+
+### How you get your [estimated] position
+
+In our SDK we provide `MotionDnaSDKDelegate` class which you implment in your viewcontroller or other class and and implement the required deegate methods.
+
 ``` Swift
 class MotionDnaManager: MotionDnaSDK {
-
-  override func receive(_ motionDna: MotionDna!) {
-    NSLog("%.8f %.8f %.8f %.8f\n", motionDna.getLocation().heading, motionDna.getLocation().localLocation.x, motionDna.getLocation().localLocation.y, motionDna.getLocation().localLocation.z)
-  }
-
-  override func receiveNetworkData(_ networkCode: NetworkCode, withPayload map: Dictionary<>) {
-  }
-
-  override func receiveNetworkData(_ motionDna: MotionDna) {
-  }
-
-  override func failure(toAuthenticate msg: String!) {
-  }
-
-  override func reportSensorTiming(_ dt: Double, msg: String!) {
-  }
+  override func receive(motionDna: MotionDna) {}
+  override func report(status: MotionDnaSDK.Status, message: String) {}
 }
 ```
 
-The ``` receive(_ motionDna: MotionDna!) ``` method is the key method in using the SDK. It will be responsible for passing location data from our core to your project. The MotionDna object contains your x,y,and z coordinates (in meters) with respect to your starting position, as well as a your current latitude and longitude and several other metrics of interest which you can read about in more detail in our [documentation](https://github.com/navisens/NaviDocs/blob/master/API.iOS.md#getters).
+The ``` receive(motionDna: MotionDna) ``` callback method returns a MotionDna estimation object containing [location, heading and motion classification](https://github.com/navisens/NaviDocs/blob/master/API.iOS.md#estimation-properties) among many other interesting data on a users current state. Here is how we might print it out.
 
-Our SDK also support location sharing through the use of a server and the startUDP() method calls. Data is received by the  ``` receiveNetworkData ``` methods. The base method is passed a MotionDna object that for each user that is also sharing with the same host, developer key, and room.
-
-The ``` reportError ``` callback received information regarding any issue that may have arisen over the course of trying to run our SDK. This can range from authentication errors with your developer key, to missing permissions that are needed for our system to run. For a full list see our documentation [here](https://github.com/navisens/NaviDocs/blob/master/API.iOS.md#reporterror_-errorcode-errorcode-withmessage-s-string).
+``` swift
+  override func receive(_ motionDna: MotionDna!) {
+    NSLog("%.8f %.8f %.8f %.8f\n",  motionDna.location.cartesian.heading,
+                                    motionDna.location.cartesian.x,
+                                    motionDna.location.cartesian.y,
+                                    motionDna.location.cartesian.z)
+  }
+```
 
 ### Running the SDK
 
-The ``` viewDidLoad ``` method of your view controller is a good place for initializing and could include the following object instantiation and method calls.
+Add the subclassed MotionDnaSDK as a property of your View Controller
 
-##### ViewController.swift
 ``` Swift
-var manager = MotionDnaManager()
-
-override func viewDidLoad() {
-  manager.receiver = self
-  manager.runMotionDna("<developer-key>")
-}
+var manager = MotionDnaSDK()
 ```
 
-Substitute ``` <developer-key> ``` with your own Navisens provided developer key. Again, if you do not yet have one then please navigate to our [developer sign up](https://www.navisens.com/index.html#contact) to request a free key.
+``` Swift
+manager.start("<developer-key>")
+```
 
-Running this project will begin to show your location estimation on the phone screen. Walk around to see how it changes your x, y, and z coordinates.
+## Common Configurations (with code examples)
+### Startup
+``` Swift
+manager.start(withDeveloperKey:"<developer-key>")
+```
+### Startup with Configuration (Model Selection)
+Additional configuration options will be added over time. Current configuration options are only for model seletion in motion estimation. Currently supported models are "standard", "headmount", and "chestmount".
 
-If you have two or more phones running with the same developer key you will see their locations appear and update in realtime at the bottom on the opposite phone
+``` Swift
+var configuration = [String:Any]()
+configuration["model"] = "standard"
+manager.start(withDeveloperKey: "<developer-key>", configurations: configuration)
+```
 
-## More Info
+### Setting SDK Options
+#### Common Task:
 
-Additional configuration options are described in the project source and our iOS [documentation](https://github.com/navisens/NaviDocs/blob/master/API.iOS.md). 
+### _Assigning initial position Locally (Cartesian X and Y coordinates)_
+#### Common Tasks:
+You know that a users position should be shifted by 4 meters in the X direction and 9 in the Y direction. Heading should not change. If the current estimated position is (4,3) the updated position should be (8,12)
+
+``` manager.setCartesianPosition(x:4, y: 9) ```
+
+
+-------------
+
+### _Assigning initial position Globally (Latitude and Longitude coordinates)_
+
+#### Common Tasks:
+ You need to update the latitude and longitude to (37.756581, -122.419155). Heading can be taken from the device's compass
+
+``` manager.setGlobalPosition(37.756581, longitude:-122.419155) ```
+
+ You know the users location is latitude and longitude of (37.756581, -122.419155) with a heading of 3 degrees and need to indicate that to the SDK
+
+``` manager.setGlobalPositionAndHeading(37.756581, longitude:-122.419155, heading:3.0) ```
+
+------------
+
+### _Observations (EXPERIMENTAL)_
+#### Common Task:
+A user is indoors and revisits the same areas frequently. Through some outside mechanism the developer is aware of a return to certain landmarks and would like to indicate that the user has returned to a landmark with ID of 38 to aid in the estimation of a user's position. The developer also knows that this observation was made within 3 meters of the landmark 38
+
+``` manager.recordObservation(withIdentifier: 38, uncertainty: 3.0) ```
+
+
+## Additional configuration options are described in the project source and our [iOS Documentation](https://github.com/navisens/NaviDocs/blob/master/API.iOS.md).
--- iOS-helloworld-swift/ViewController.swift
+++ iOS-helloworld-swift/ViewController.swift
@@ -9,66 +9,67 @@
 import UIKit
 import MotionDnaSDK
 
-func motionTypeToString(motionType: MotionType ) -> String? {
-    switch (motionType) {
-    case STATIONARY:
-        return "STATIONARY";
-    case FIDGETING:
-        return "FIDGETING";
-    case FORWARD:
-        return "FORWARD";
-    default:
-        return "UNKNOWN MOTION";
-    }
-}
 
 struct MotionData {
     var id:String
     var deviceName:String
     var location:Location
-    var motionType:MotionType
 }
 
-class ViewController: UIViewController {
-    var manager = MotionDnaManager()
+class ViewController: UIViewController, MotionDnaSDKDelegate {
+    var motionDnaSDK : MotionDnaSDK!
     
     @IBOutlet weak var receiveMotionDnaTextField: UITextView!
-    @IBOutlet weak var receiveNetworkDataTextField: UITextView!
-    
-    var networkUsers:[String:MotionData] = [:]
-    var networkUsersTimestamps:[String:Double] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startMotionDna();
+        startDemo();
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    func receive(_ motionDna: MotionDna!) {
-        let location = motionDna.getLocation()
-        let localLocation = location.localLocation
-        let globalLocation = location.globalLocation
-        let motion = motionDna.getMotion()
+    func receive(motionDna: MotionDna) {
+        
+        
+        let location = motionDna.location!
+        let localLocation = location.cartesian!
+        let globalLocation = location.global!
+        let motion = motionDna.classifiers["motion"]?.prediction.label
 
         let motionDnaLocalString = String(format:"Local XYZ Coordinates (meters) \n(%.2f,%.2f,%.2f)",localLocation.x,localLocation.y,localLocation.z)
-        let motionDnaHeadingString = String(format:"Current Heading: %.2f",location.heading)
+        let motionDnaHeadingString = String(format:"Current Heading: %.2f",location.global.heading)
         let motionDnaGlobalString = String(format:"Global Position: \n(Lat: %.6f, Lon: %.6f)",globalLocation.latitude,globalLocation.longitude)
-        let motionDnaMotionTypeString = String(format:"Motion Type: %@",motionTypeToString(motionType: motion.motionType)!)
+        let motionDnaMotionTypeString = String(format:"Motion Type: %@",motion!)
 
-        guard let classifiers = motionDna.getClassifiers() else {
+        guard let classifiers = motionDna.classifiers else {
             return;
         }
         var motionDnaPredictionString = "Predictions (BETA):\n"
+        var classifierNames = [String]()
+        for (classifierName, _) in classifiers {
+            classifierNames.append(classifierName);
+        }
+        classifierNames.sort()
         
-        for (classifierName, classifier) in classifiers {
+        for classifierName in classifierNames {
+            guard let classifier = classifiers[classifierName] else {
+                break;
+            }
             motionDnaPredictionString.append(String(format: "Classifier: %@\n", classifierName))
-            motionDnaPredictionString.append(String(format: "\t prediction: %@ confidence: %.2f\n", classifier.currentPredictionLabel,classifier.currentPredictionConfidence))
-                
-            for (predictionLabel, predictionStats) in classifier.predictionStats {
+            motionDnaPredictionString.append(String(format: "\t prediction: %@ confidence: %.2f\n", classifier.prediction.label,classifier.prediction.confidence))
+            
+            var predictionLabels = [String]()
+            for (predictionLabel, _) in classifier.statistics {
+                predictionLabels.append(predictionLabel);
+            }
+            predictionLabels.sort()
+            for predictionLabel in predictionLabels {
+                guard let predictionStats = classifier.statistics[predictionLabel] else {
+                    break;
+                }
                 motionDnaPredictionString.append(String(format: "\t%@\n", predictionLabel))
                 motionDnaPredictionString.append(String(format: "\t duration: %.2f\n", predictionStats.duration))
                 motionDnaPredictionString.append(String(format: "\t distance: %.2f\n", predictionStats.distance))
@@ -76,7 +77,7 @@ class ViewController: UIViewController {
             motionDnaPredictionString.append("\n")
         }
         
-        let motionDnaString = String(format:"MotionDna Location:\n%@\n%@\n%@\n%@\n%@",motionDnaLocalString,
+        let motionDnaString = String(format:"%@\n MotionDna Location:\n%@\n%@\n%@\n%@\n%@",MotionDnaSDK.sdkBuild(),motionDnaLocalString,
                                      motionDnaHeadingString,
                                      motionDnaGlobalString,
                                      motionDnaMotionTypeString,
@@ -86,110 +87,35 @@ class ViewController: UIViewController {
         }
     }
 
-    func receiveNetworkData(_ motionDna: MotionDna!) {
-        let motionData = MotionData(id: motionDna.getID(),
-                                    deviceName: motionDna.getDeviceName(),
-                                    location: motionDna.getLocation(),
-                                    motionType: motionDna.getMotion().motionType)
-        networkUsers[motionDna.getID()] = motionData
-        let timeSinceBootSeconds = ProcessInfo.processInfo.systemUptime
-        networkUsersTimestamps[motionDna.getID()] = timeSinceBootSeconds
-        var activeNetworkUsersString = String()
-        var toRemove = [String]()
+    
 
-        activeNetworkUsersString.append("Network Shared Devices:\n")
-
-        for ( _, user) in networkUsers {
-            if (timeSinceBootSeconds - networkUsersTimestamps[user.id]! > 2.0) {
-                toRemove.append(user.id)
-            } else {
-                activeNetworkUsersString = activeNetworkUsersString.appending(user.deviceName.components(separatedBy:";").last!)
-                let location = user.location.localLocation
-                activeNetworkUsersString = activeNetworkUsersString.appending(String(format:" (%.2f, %.2f, %.2f)\n",location.x, location.y, location.z))
-            }
-        }
-
-        for key in toRemove {
-            networkUsers.removeValue(forKey: key)
-            networkUsersTimestamps.removeValue(forKey: key)
-        }
-        DispatchQueue.main.async {
-            self.receiveNetworkDataTextField.text = activeNetworkUsersString
+    func report( status: MotionDnaSDK.Status, message: String) {
+        switch status {
+        case .sensorTimingIssue:
+            print("Error: Sensor Timing "  + message)
+        case .authenticationFailure:
+            print("Error: Authentication Failed " + message)
+        case .missingSensor:
+            print("Error: Sensor Missing " + message)
+        case .expiredSDK:
+            print("Error: SDK Expired " + message)
+        case .authenticationSuccess:
+            print("Status Authenticated: " + message)
+        case .permissionsFailure:
+            print("Status Permissions: " + message)
+        case .none:
+            print("Status None: " + message)
+        default:
+            print("Error Unknown: " + message)
         }
     }
+    
 
-    func receiveNetworkData(_ opcode: NetworkCode, withPayload payload: [AnyHashable : Any]!) {
-
-    }
-
-    func startMotionDna() {
-
-        manager.receiver = self;
-
-        //    This functions starts up the SDK. You must pass in a valid developer's key in order for
-        //    the SDK to function. IF the key has expired or there are other errors, you may receive
-        //    those errors through the reportError() callback route.
-        
-        manager.runMotionDna("<developer-key>")
-
-        //    Use our internal algorithm to automatically compute your location and heading by fusing
-        //    inertial estimation with global location information. This is designed for outdoor use and
-        //    will not compute a position when indoors. Solving location requires the user to be walking
-        //    outdoors. Depending on the quality of the global location, this may only require as little
-        //    as 10 meters of walking outdoors.
-
-        manager.setLocationNavisens()
-
-        //   Set accuracy for GPS positioning, states :HIGH/LOW_ACCURACY/OFF, OFF consumes
-        //   the least battery.
-
-        manager.setExternalPositioningState(LOW_ACCURACY)
-
-        //    Manually sets the global latitude, longitude, and heading. This enables receiving a
-        //    latitude and longitude instead of cartesian coordinates. Use this if you have other
-        //    sources of information (for example, user-defined address), and need readings more
-        //    accurate than GPS can provide.
-//        manager.setLocationLatitude(37.787582, longitude: -122.396627, andHeadingInDegrees: 0.0)
-
-        //    Set the power consumption mode to trade off accuracy of predictions for power saving.
-
-        manager.setPowerMode(PERFORMANCE)
-
-        //    Connect to your own server and specify a room. Any other device connected to the same room
-        //    and also under the same developer will receive any udp packets this device sends.
-
-        manager.startUDP()
-
-        //    Allow our SDK to record data and use it to enhance our estimation system.
-        //    Send this file to support@navisens.com if you have any issues with the estimation
-        //    that you would like to have us analyze.
-
-        manager.setBinaryFileLoggingEnabled(true)
-
-        //    Tell our SDK how often to provide estimation results. Note that there is a limit on how
-        //    fast our SDK can provide results, but usually setting a slower update rate improves results.
-        //    Setting the rate to 0ms will output estimation results at our maximum rate.
-
-        manager.setCallbackUpdateRateInMs(500)
-
-        //    When setLocationNavisens is enabled and setBackpropagationEnabled is called, once Navisens
-        //    has initialized you will not only get the current position, but also a set of latitude
-        //    longitude coordinates which lead back to the start position (where the SDK/App was started).
-        //    This is useful to determine which building and even where inside a building the
-        //    person started, or where the person exited a vehicle (e.g. the vehicle parking spot or the
-        //    location of a drop-off).
-
-        manager.setBackpropagationEnabled(true)
-
-        //    If the user wants to see everything that happened before Navisens found an initial
-        //    position, he can adjust the amount of the trajectory to see before the initial
-        //    position was set automatically.
-
-        manager.setBackpropagationBufferSize(2000)
-
-        //    Enables AR mode. AR mode publishes orientation quaternion at a higher rate.
-
-//        manager.setARModeEnabled(true)
+    
+    func startDemo() {
+        motionDnaSDK = MotionDnaSDK(delegate: self)
+        let developerKey = "<--DEVELOPER-KEY-HERE-->"
+        motionDnaSDK.start(withDeveloperKey: developerKey)
     }
 }
 
